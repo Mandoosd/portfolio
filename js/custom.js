a3f8b280599ebb9a6aacd634622052e1be5ec4a7
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-03-28 16:54:40 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,83 +1,7 @@
 //즉시실행함수
 (function () {
     contentFullpage();
-    coverTimeline();
-    gnbEvent();
-    circleTxt(`.profile .ctxt`);
 })();
-
-particlesJS("particles-js", {
-    particles: {
-        number: { value: 6, density: { enable: true, value_area: 800 } },
-        color: { value: "#000000" },
-        shape: {
-            type: "edge",
-            stroke: { width: 0, color: "#000" },
-            polygon: { nb_sides: 6 },
-            image: { src: "img/github.svg", width: 100, height: 100 },
-        },
-        opacity: {
-            value: 0.1183721462448409,
-            random: true,
-            anim: { enable: false, speed: 0, opacity_min: 0.1, sync: false },
-        },
-        size: {
-            value: 160,
-            random: false,
-            anim: { enable: true, speed: 10, size_min: 40, sync: false },
-        },
-        move: {
-            enable: true,
-            speed: 8,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: { enable: false, rotateX: 600, rotateY: 1200 },
-        },
-    },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: { enable: true, mode: "grab" },
-            onclick: { enable: false, mode: "push" },
-            resize: true,
-        },
-        modes: {
-            grab: { distance: 400, line_linked: { opacity: 0 } },
-            bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-            },
-            repulse: { distance: 200, duration: 0.4 },
-            push: { particles_nb: 4 },
-            remove: { particles_nb: 2 },
-        },
-    },
-    retina_detect: true,
-});
-var count_particles, stats, update;
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-    stats.begin();
-    stats.end();
-    if (
-        window.pJSDom[0].pJS.particles &&
-        window.pJSDom[0].pJS.particles.array
-    ) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-};
 
 requestAnimationFrame(update); //프레임전체 fullpage
 function contentFullpage() {
@@ -97,14 +21,28 @@ function contentFullpage() {
         },
 
         onLeave: (origin, destination, direction, trigger) => {
-            destination.index === 0 && mainVisualTimeline();
+            if (destination.index === 0) {
+                document.querySelector(".gnb").classList.add("on");
+            } else {
+                // gnb에서 'on' 클래스 삭제
+                document.querySelector(".gnb").classList.remove("on");
+            }
 
-            destination.index === 1 && storyTimeline();
-            destination.index === 2 && profileTimeline();
+            // gnb li에 active 클래스 추가/제거
+            const gnbItems = document.querySelectorAll(".gnb li");
+            gnbItems.forEach((item, index) => {
+                if (index === destination.index) {
+                    item.classList.add("active");
+                } else {
+                    item.classList.remove("active");
+                }
+            });
+
+            destination.index === 0 && mainVisualTimeline();
         },
 
         onSlideLeave: (section, origin, destination, direction, trigger) => {
-            destination.index && portfolioTimeline(destination.index - 1);
+            destination.index && portfolioTimeline(destination.index);
         },
     });
 
@@ -120,63 +58,66 @@ function contentFullpage() {
     });
 }
 
-function coverTimeline() {
-    const btn = document.querySelector(".utils_wrap .btn");
-
-    const cover = document.querySelector(".gnb");
-
-    btn.addEventListener("click", (e) => {
-        const t = e.currentTarget;
-        t.classList.toggle("is-active");
-        cover.classList.toggle("on");
-    });
-}
-
-function gnbEvent() {
-    const cover = document.querySelector(".gnb");
-    const btn = document.querySelector(".utils_wrap .btn");
-    cover.addEventListener("wheel", (e) => {
-        //e.preventDefault();
-        e.stopPropagation(); // 이벤트의 전파를 막음
-    });
-
-    const lnk = document.querySelectorAll(".gnb .lnk a");
-
-    lnk.forEach((it, idx) => {
-        it.addEventListener("click", () => {
-            cover.classList.remove("on");
-            btn.classList.remove("is-active");
-        });
-    });
-}
-
 function mainVisualTimeline() {
+    const v = document.querySelector(".mainVisal .painting video");
     const t = document.querySelector(".mainVisal .tit h2");
-    const p = document.querySelector(".mainVisal .tit p");
-    const a = document.querySelector(".mainVisal .t");
-    const b = document.querySelector(".mainVisal .bg");
+    const w = document.querySelector(".mainVisal .bg ");
+    const c = document.querySelector(".mainVisal .color_bg .color_01");
+    const c01 = document.querySelector(".mainVisal .color_bg .color_02");
+    const c02 = document.querySelector(".mainVisal .color_bg .color_03");
+    const c03 = document.querySelector(".mainVisal .color_bg .color_04");
+    const c04 = document.querySelector(".mainVisal .color_bg .color_05");
+    const c05 = document.querySelector(".mainVisal .color_bg .wood");
 
     const tl = gsap.timeline();
+    tl.to(v, {
+        duration: 0.1, // 비디오를 바로 재생시키기 위해 매우 짧은 시간 설정
+        play: 0, // 비디오를 재생
+    });
     tl.from(t, {
-        x: 1000,
-        autoAlpha: 0,
+        x: 0,
         delay: 2,
         duration: 0.5,
     });
-    tl.from(b, {
-        x: 1000,
-        rotate: 180,
-        autoAlpha: 0,
-        delay: 0.5,
-        duration: 0.5,
+    tl.from(w, {
+        delay: -0.1,
+        duration: 0.7,
+    });
+    tl.from(c, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
+    });
+    tl.from(c01, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
+    });
+    tl.from(c02, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
+    });
+    tl.from(c03, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
+    });
+    tl.from(c04, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
+    });
+    tl.from(c05, {
+        delay: -0.1,
+        duration: 0.3,
+        opacity: 0,
     });
 }
+
 function portfolioTimeline(n) {
-    const t = document.querySelectorAll(".portfolio .desc h3");
-    const p = document.querySelectorAll(".portfolio .desc p");
+    const t = document.querySelectorAll(".portfolio .desc ");
     const m = document.querySelectorAll(".portfolio .mokup");
-    const tb = document.querySelectorAll(".portfolio .table");
-    const lnk = document.querySelectorAll(".portfolio .link");
     const b = document.querySelectorAll(".portfolio .brush_inner");
 
     const tl = gsap.timeline();
@@ -191,19 +132,7 @@ function portfolioTimeline(n) {
             x: 200,
             duration: 0.3,
         })
-        .from(p[n], {
-            autoAlpha: 0,
-            x: 200,
-            duration: 0.3,
-        })
-        .from(tb[n], {
-            autoAlpha: 0,
-            x: 200,
-        })
-        .from(lnk[n], {
-            autoAlpha: 0,
-            x: 200,
-        })
+
         .fromTo(
             b[n],
             { width: "100%", left: "0" }, // 오른쪽 끝을 기준으로 시작하도록 설정
@@ -211,27 +140,9 @@ function portfolioTimeline(n) {
         );
 }
 
-function storyTimeline() {
-    const t = document.querySelector(".portfolio .tit h3");
-    const p = document.querySelector(".portfolio .tit p");
-
-    const tl = gsap.timeline();
-
-    tl.from(t, {
-        autoAlpha: 0,
-        x: 2000,
-        delay: 0.5,
-    }).from(p, {
-        autoAlpha: 0,
-        x: 2000,
-        delay: 0.5,
-    });
-}
-
 function profileTimeline() {
     const t = document.querySelector(".profile .tit h3");
     const p = document.querySelector(".profile .tit p");
-    const c = document.querySelector(".profile .ctxt");
     const tl = gsap.timeline();
 
     tl.from(t, {
@@ -242,38 +153,5 @@ function profileTimeline() {
         x: 1000,
         autoAlpha: 0,
         delay: 1,
-    });
-}
-
-function mouseCursor() {
-    const mc = document.querySelectorAll(".mainVisal , .profile");
-    mc.forEach((it) => {
-        it.addEventListener("mousemove", (e) => {
-            let mouseX = e.pageX + 10; // document의 x좌표값
-            let mouseY = e.pageY + 10; // document의 y좌표값
-
-            const cursor = document.querySelector(".cursor");
-            cursor.style.left = mouseX + "px";
-            cursor.style.top = mouseY + "px";
-        });
-    });
-}
-
-function circleTxt(txt) {
-    const t = document.querySelector(txt).innerText;
-    const at = [...t].map((it) => `<span>${it}</span>`).join("");
-    document.querySelector(txt).innerHTML = at;
-    const b = document.querySelector(txt);
-    const ctxt = b.querySelectorAll("span");
-    console.log(ctxt);
-
-    ctxt.forEach((it, idx, arry) => {
-        it.style.cssText = `
-        position: absolute;
-        top:0;
-        left: 50%;
-        height: 100%;
-        transform: translate(-50%,0) rotate(${(360 / arry.length) * idx}deg)
-        `;
     });
 }
